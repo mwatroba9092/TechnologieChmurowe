d6b39b85d55d2fb693c5197c1adf80e1fcf2c014
=michal_watroba <=m.watroba.046@studms.ug.edu.pl>
2026-03-21 12:34:59 +0100

    zadanie2

---
+++ lab03/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  if (req.method === 'GET' && req.url === '/health') {
+    res.writeHead(200);
+    res.end(JSON.stringify({ status: 'ok', service: 'backend-api' }));
+  } 
+
+  else if (req.method === 'GET' && req.url === '/items') {
+    res.writeHead(200);
+    res.end(JSON.stringify({ items: ['item1', 'item2', 'item3'] }));
+  } 
+});
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000/");
+})
